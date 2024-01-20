5dfcc2dfab3311a609ab1e5747b2856fabbdd3e4
Ritesh Parashar <84569396+riteshparashar52@users.noreply.github.com>
2024-01-20 17:46:40 +0530

    Add files via upload

---
+++ s3.py
@@ -0,0 +1,24 @@
+from boto3.session import Session
+
+ACCESS_KEY_ID = 'AKIA2UC27NWWLOKUFJFD'
+SECRET_KEY = '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'
+
+session = Session(aws_access_key_id=ACCESS_KEY_ID, 
+                  aws_secret_access_key=SECRET_KEY)
+
+s3 = session.resource('s3')
+bucket = "ritesh1buck"
+my_bucket = s3.Bucket(bucket)
+print("++++++ Object before upload+++++++\n")
+
+for s3_files in my_bucket.objects.all():
+   print(s3_files.key)
+
+file_upload = '/Users/Ritesh/Desktop/Ritesh parashar/aadhar_back.jpeg'
+object_name = 'aadhar_back.jpeg'
+my_bucket.upload_file(file_upload,object_name)
+print ( "\n+++++++object after upload+++++++\n")
+
+for s3_files in my_bucket.objects.all():
+   print(s3_files.key)
+
